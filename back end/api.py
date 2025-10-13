dd0a7a735660fd482c773d150433e9b77931f04e
Gabriel Santos Silva <gabriel.s.silva95@aluno.senai.br>
2025-10-13 15:58:51 -0300

    rota adicionar filme criada em api

---
+++ back end/api.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import funcao
+
+#Rodar o fastapi:
+# python -m uvicorn api:app --reload
+
+#testar api FastAPI
+#/docs > documentação swagger
+#/redoc > documentação redoc
+
+#GET = pegar / Listar
+#POST = Criar / Enviar
+#PUT = Atualizar
+#DELETE = Deletar
+
+#Iniciar o fastapi
+app = FastAPI(title="Gerenciador de Filmes") 
+
+@app.get("/")
+def home():
+    return {"mensagem": "Quero ir para casa🏠"}
+
+
+@app.post("/filmes")
+def criar_filmes(titulo: str, genero: str, ano: int, avaliacao: float):
+    funcao.inserir_filme(titulo, genero, ano, avaliacao)
+    return{"mensagem": "Filme adicionado com sucesso"}
+
+@app.get("/filmes")
+def listar_filmes():
+    filmes = funcao.listar_filme()
+    
